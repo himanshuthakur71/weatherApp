98b723b2f146c46fe6728f797718e96814551e5c
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-09-09 15:19:02 +0530

    Addded

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -2,7 +2,7 @@ import { fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 
 export const actions: Actions = {
-    login: async ({ request }) => {
+    login: async ({ request, locals: { supabase } }) => {
         const formData = await request.formData();
         const email = formData.get("email")?.toString().trim();
         const password = formData.get("password")?.toString();
@@ -10,6 +10,13 @@ export const actions: Actions = {
 
         if (!email || !password) {
             return fail(400, { error: "Email and password are required" });
+        }
+
+        const { error } = await supabase.auth.signInWithPassword({ email, password });
+        if (error) {
+            return fail(400, { error: error?.message || "Login failed" });
+        } else {
+            redirect(303, '/dashboard')
         }
 
     }
