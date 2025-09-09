1b7d287fc2a3e4fd9ca66c141afd1e370d19a0fa
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-09-09 14:58:12 +0530

    Sign in html added

---
+++ src/routes/auth/login/+page.server.ts
@@ -0,0 +1,20 @@
+import { fail, redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+
+export const actions: Actions = {
+    login: async ({ request, cookies }) => {
+        const formData = await request.formData();
+        const email = formData.get("email")?.toString().trim();
+        const password = formData.get("password")?.toString();
+        const redirectTo = formData.get("redirectTo")?.toString() || "/dashboard";
+
+        if (!email || !password) {
+            return fail(400, { error: "Email and password are required" });
+        }
+
+
+
+
+        return fail(500, { error: "Unexpected login error" });
+    }
+};
