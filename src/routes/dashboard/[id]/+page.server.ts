6ff189beef67f490a62670854cf85b01abb00f86
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-09-09 20:45:38 +0530

    Addded

---
+++ src/routes/dashboard/[id]/+page.server.ts
@@ -0,0 +1,26 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+
+export const load: PageServerLoad = async ({ params, locals: { user, supabase } }) => {
+
+
+    // Fatch user weathers 
+    const getUserWeather = async () => {
+        let { data: weather, error } = await supabase
+            .from('weathers')
+            .select("*")
+            // Filters
+            .eq('id', params.id)
+            .single()
+
+        return weather || null
+    }
+
+
+    return {
+        weathers: await getUserWeather()
+    }
+
+
+}
