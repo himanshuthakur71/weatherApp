2d9e19817426215cdd9d5c1788f34e81ca7e9000
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-09-09 20:07:59 +0530

    addded

--- src/routes/dashboard/current/+page.ts
+++ src/routes/dashboard/current/+page.ts
@@ -1,11 +1,16 @@
 import { browser } from "$app/environment";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = async ({fetch}) => {
+export const load: PageLoad = async ({ fetch, parent }) => {
+
+    // call parent() to get subabase client
+    const { supabase, user } = await parent()
+
+
     let weatherData = null;
     if (browser) {
         try {
-            // ✅ Get geolocation only on client
+            // Get geolocation only on client
             const position = await new Promise<GeolocationPosition>((resolve, reject) =>
                 navigator.geolocation.getCurrentPosition(resolve, reject)
             );
@@ -14,6 +19,30 @@ export const load: PageLoad = async ({fetch}) => {
             // Fetch weather from server route
             const res = await fetch(`/api/weather?lat=${latitude}&lon=${longitude}`);
             weatherData = await res.json();
+
+            if (weatherData) {
+
+                const payload = {
+                    user_id: user?.id || '',
+                    name: weatherData?.name || '',
+                    sys: weatherData?.sys || null,
+                    coord: weatherData?.coord || null,
+                    weather: weatherData?.weather || null,
+                    main: weatherData?.main || null,
+                    wind: weatherData?.wind || null,
+                    visibility: weatherData?.visibility || null,
+                    clouds: weatherData?.clouds || null,
+                    auth_user_id: user?.id || '',
+                }
+
+                // save data tp supabase
+                const { data: w_data, error: w_err } = await supabase
+                    .from('weathers')
+                    .insert([
+                        payload,
+                    ])
+            }
+
         } catch (err) {
             console.error("Failed to get location or weather:", err);
         }
@@ -22,7 +51,7 @@ export const load: PageLoad = async ({fetch}) => {
         weatherData = null; // or provide default lat/lon if you want
     }
 
-    return{
+    return {
         weatherData
     }
 };
