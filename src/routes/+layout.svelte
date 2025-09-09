1154713d1ed7024eb802835965403fb32e4c5f20
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-09-09 15:13:10 +0530

    supbase pass to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 
-	let { children } = $props();
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-
-<div class="w-full h-full min-h-screen grid grid-rows-[auto_1fr_auto]">
+<div class="grid h-full min-h-screen w-full grid-rows-[auto_1fr_auto]">
 	<Navbar />
 	<main>
 		{@render children?.()}
@@ -20,5 +31,3 @@
 
 	<Footer />
 </div>
-
-
