53a288a96bba492b0b04cae47a8ebadfda2f170b
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-09-10 14:10:25 +0530

    icon addded

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -8,8 +8,17 @@
 		// Import leaflet only on client
 		const L = await import('leaflet');
 
+		const customIcon = L.icon({
+			iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+			iconSize: [32, 32],
+			iconAnchor: [16, 32],
+			popupAnchor: [0, -32],
+		});
+
 		// Create map
 		const map = L.map(mapDiv).setView([lat, lng], 18);
+
+		L.marker([lat, lng], { icon: customIcon }).addTo(map);
 
 		// Add free satellite tiles (ESRI World Imagery)
 		L.tileLayer(
@@ -24,6 +33,4 @@
 	});
 </script>
 
-<div bind:this={mapDiv} id="map"  class="w-full h-[220px]"></div>
-
-
+<div bind:this={mapDiv} id="map" class="h-[220px] w-full"></div>
